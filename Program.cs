fef692435f33f2d06a35e09707579926706327fe
H. TAKI <Taki.Hicham.01@outlook.com>
2023-06-11 16:36:22 +0200

    Adding sonar csharp analyzers and Directory.Build.props

--- Program.cs
+++ Program.cs
@@ -1,24 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace ProjectSetup;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal sealed class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private Program()
+    {
+        
+    }
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+
+
+        app.MapGet("/hello", () => "Hello world !");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-
-
-app.MapGet("/hello", () => "Hello world !");
-
-app.Run();
-
